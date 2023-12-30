7274f45fb5fdabc6178ca003396e516f5be051ba
Furkan Avcı <furkanavci039@gmail.com>
2023-12-30 10:13:40 +0300

    .env added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    NEXT_SERVER_URL : process.env.NEXT_SERVER_URL,
+    GRAPHQL_SERVER_URL : process.env.GRAPHQL_SERVER_URL
+  },
   images:{
     remotePatterns: [
       {

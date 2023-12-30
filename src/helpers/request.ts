9d26a714cf649eff6622c451a9f5c6030e984b01
Furkan Avcı <furkanavci039@gmail.com>
2023-12-30 13:22:15 +0300

    typeof window !== 'undefined' added

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,7 +1,10 @@
 import { NEXT_SERVER_URL } from '@/constants'
 
 export const apiGraphqlRequest = <T = unknown>(body: string): Promise<{ data: T }> =>
-  fetch(`${window?.location?.origin ?? NEXT_SERVER_URL}/api/graphql`, {
-    method: 'POST',
-    body
-  }).then((res) => res.json())
+  fetch(
+    `${typeof window !== 'undefined' ? window?.location?.origin : NEXT_SERVER_URL}/api/graphql`,
+    {
+      method: 'POST',
+      body
+    }
+  ).then((res) => res.json())

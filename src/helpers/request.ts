c7a399bcd54e8d57440b2f8037f5cde537bb24f2
Furkan Avcı <furkanavci039@gmail.com>
2023-12-30 13:18:51 +0300

    request url reading from window.location

--- src/helpers/request.ts
+++ src/helpers/request.ts
@@ -1,7 +1,7 @@
-import { NEXT_SERVER_URL, GRAPHQL_SERVER_URL } from '@/constants'
+import { NEXT_SERVER_URL } from '@/constants'
 
 export const apiGraphqlRequest = <T = unknown>(body: string): Promise<{ data: T }> =>
-  fetch(`${NEXT_SERVER_URL}/api/graphql`, {
+  fetch(`${window?.location?.origin ?? NEXT_SERVER_URL}/api/graphql`, {
     method: 'POST',
     body
   }).then((res) => res.json())

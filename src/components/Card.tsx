b6e0f497222b5b3db4c583e8c06eab95bda0ead0
furkanavc1 <furkanavci039@gmail.com>
2024-01-02 11:54:02 +0300

    card style changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,8 +10,33 @@ type CardProps = {
 }
 export const Card = ({ person, onMinusClick, onPlusClick }: CardProps) => {
   return (
-    <div className={'relative flex h-96 w-full flex-col overflow-hidden rounded-md border'}>
-      <div className="relative h-full w-full">
+    // <div className={'relative flex h-96 w-full flex-col overflow-hidden rounded-md border'}>
+    //   <div className="relative h-full w-full">
+    //     <Image
+    //       fill
+    //       alt="image"
+    //       className="object-cover"
+    //       src={person?.image ?? 'https://picsum.photos/200/300'}
+    //     />
+    //   </div>
+    //   <div>{person?.fullName}</div>
+    //   <div>{person?.voteCount}</div>
+    //   <div className="flex h-12 items-center justify-around bg-black">
+    //     <span
+    //       onClick={onMinusClick}
+    //       className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
+    //       -
+    //     </span>
+    //     <span
+    //       onClick={onPlusClick}
+    //       className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
+    //       +
+    //     </span>
+    //   </div>
+    // </div>
+
+    <div className="flex h-96 w-full flex-col overflow-hidden rounded-lg   border border-gray-700 bg-gray-800 shadow">
+      <div className="relative h-full w-full ">
         <Image
           fill
           alt="image"
@@ -19,20 +44,30 @@ export const Card = ({ person, onMinusClick, onPlusClick }: CardProps) => {
           src={person?.image ?? 'https://picsum.photos/200/300'}
         />
       </div>
-      <div>{person?.fullName}</div>
-      <div>{person?.voteCount}</div>
-      <div className="flex h-12 items-center justify-around bg-black">
-        <span
-          onClick={onMinusClick}
-          className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
-          -
-        </span>
-        <Link href={`/detail/${person?.id || ''}`}>Detail</Link>
-        <span
-          onClick={onPlusClick}
-          className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
-          +
-        </span>
+      <div className="p-5">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          {person?.fullName}
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          Vote Count:{person?.voteCount}
+        </p>
+        <Link
+          href={`/detail/${person?.id || ''}`}
+          className="inline-flex items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300  ">
+          Detail
+        </Link>
+        <div className="flex h-12 items-center space-x-2">
+          <button
+            onClick={onMinusClick}
+            className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
+            -
+          </button>
+          <button
+            onClick={onPlusClick}
+            className="flex size-10 items-center justify-center rounded-md bg-slate-400 text-lg hover:bg-slate-300">
+            +
+          </button>
+        </div>
       </div>
     </div>
   )

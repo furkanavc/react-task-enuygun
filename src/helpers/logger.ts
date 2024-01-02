2e01955e67fdc06feb7e47d0c1b1696566cce8c7
furkanavc1 <furkanavci039@gmail.com>
2024-01-02 14:29:14 +0300

    logtoserver added

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,7 +1,15 @@
 import { Person } from '@/types/Person'
 
+const logToServer = (msg: string) => {
+  console.log(msg)
+}
+
 export const voteLogger = (person: Person, str?: string) => {
   str === '+'
-    ? console.log(`Voted for ${person?.fullName} new vote count is ${person?.voteCount + 1}`)
-    : console.log(`Unvoted for ${person?.fullName} new vote count is ${person?.voteCount - 1}`)
+    ? logToServer(`Voted for ${person?.fullName} new vote count is ${person?.voteCount + 1}`)
+    : logToServer(`Unvoted for ${person?.fullName} new vote count is ${person?.voteCount - 1}`)
+}
+
+export const routeLogger = (url: string) => {
+  logToServer(`App is changing to ${url}`)
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { routeLogger } from '@/helpers/logger'
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter()
 
   useEffect(() => {
-    const handleRouteChange = (url: string) => {
-      console.log(`App is changing to ${url}`)
-    }
-    router.events.on('routeChangeStart', handleRouteChange)
-
+    router.events.on('routeChangeStart', routeLogger)
     return () => {
-      router.events.off('routeChangeStart', handleRouteChange)
+      router.events.off('routeChangeStart', routeLogger)
     }
   }, [router])
   return <Component {...pageProps} />
